4c56d3b6bb14c49dfaebb087348b7d6fec00f00a
unknown <mash@MSHEPHERD.local.tmvse.com>
2015-09-25 11:33:29 +0100

    Updating site

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-hugo -t shiori
+hugo -t shiori-custom
 
 # Add everything
 git add -A
@@ -8,3 +8,4 @@ git commit -m "Updating site" && git push origin blog
 
 # Push the public subtree to the gh-pages branch
 git subtree push --prefix=public https://github.com/MistaGiggles/mistagiggles.github.io master --squash
+git subtree push --prefix=public https://github.com/MistaGiggles/hugo-theme-shiori master --squash
